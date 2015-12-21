866afdde835420aae79fa05435f6b16214f6d6d4
Igorovitch <cermenoanabelle@hotmail.com>
2015-12-21 10:25:06 +0100

    Automatic update to make ScraperWiki scraper work on morph.io

--- scraper.php
+++ scraper.php
@@ -1,4 +1,5 @@
 <?php
+require 'scraperwiki.php';
 $url = 'http://us.megabus.com/JourneyResults.aspx?originCode=95&destinationCode=123&outboundDepartureDate=2%2f18%2f2013&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=TRYMEGABUS&withReturn=0';
 
 $ch = curl_init($url);
